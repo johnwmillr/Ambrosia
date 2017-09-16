c6095f17eac2047d69cde05dade5be42bfe03d16
Ahmed Elmaleh <ahmed-elmaleh@uiowa.edu>
2017-09-16 14:56:03 -0500

    adding parser

---
+++ ambrosia/__init__.py
@@ -0,0 +1,10 @@
+# Ambrosia
+# Ahmed Elmaleh, John W. Miller, Qingyang Su
+# See LICENSE for details
+# 2017-09-16
+
+"""A food library"""
+
+__author__ = "Ahmed Elmaleh, John W. Miller, and Qingyang Su"
+
+from ambrosia.api import API
---
+++ ambrosia/parser.py
@@ -0,0 +1,18 @@
+# Ambrosia
+# Ahmed Elmaleh, John W. Miller, Qingyang Su
+# See LICENSE for details
+# 2017-09-16
+
+"""A food library"""
+
+from __future__ import print_function
+
+
+class parser(object):
+    """Ambrosia Ingredient parser"""
+
+    def __init__(self):
+        data_loc="../data"
+        
+
+
